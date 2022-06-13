1ac34802e7984821fc2c1a0c378dd6e949919104
Kalmix <87293493+kalmix@users.noreply.github.com>
2022-06-13 08:44:27 -0400

    Create nxnmatrix.cpp

---
+++ nxnmatrix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+
+int
+main()
+{
+	int a[4][5], i, j;
+	for (i = 0; i < 4; ++i)
+	{
+		for (j = 0; j < 5; ++j)
+		{
+			a[i][j] = rand() % 10 + 1;
+		}
+	}
+
+	for (i = 0; i < 4; ++i)
+	{
+		for (j = 0; j < 5; ++j)
+			std::cout << a[i][j] << '\t';
+		std::cout << '\n';
+	}
+
+	return 0;
+}
